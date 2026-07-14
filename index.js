0899473a5af734280e33b83f5225d7ce5ac07f21
Anup Kumar <kumaranup2505@gmail.com>
2026-07-14 15:34:46 +0530

    add test route

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import morgan from "morgan";
 import notificationRoutes from "./src/routes/notification.routes.js";
 import "./src/workers/email.worker.js";
+import { verifyEmailService } from "./src/services/email.services.js";
+
 const app = express();
 app.use(morgan("dev"));
 app.use(cors());
@@ -19,4 +21,17 @@ app.get("/", (req, res) => {
 });
 app.use("/api/v1/notifications", notificationRoutes);
 
+app.get("/verify-email", async (req, res) => {
+  try {
+    await verifyEmailService();
+    res.json({ success: true });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: err.message,
+      code: err.code,
+      stack: err.stack,
+    });
+  }
+});
 export default app;
